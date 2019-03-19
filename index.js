aea707f414ad60db078113d9efa62267c84bf5e1
Pavel <example@mail.com>
2019-03-19 12:05:44 +0300

    Bufget accounting

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-{
-    var lol = 1;
-}
+var money, time;
+monye = +prompt("Ваш бюджет на месяц?", "");
+time = prompt("Введите дату в формате YYYY-MM-DD", "");
+article = prompt("Введите обязательную статью расходов в этом месяце", "");
+articleMoney = prompt("“Во сколько обойдется?”", "");
 
-let kek = 2;
-console.log(lol);
+var appData = {
+    budget: money,
+    timeData: time,
+    expenses: {
+        article: articleMoney
+    },
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+alert("Бюджет на 1 день" + money / 30);
