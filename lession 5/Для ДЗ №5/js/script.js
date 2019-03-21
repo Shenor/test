4c64bdfc09fff9e185950db85b8c7ca84834841e
Pavel <example@mail.com>
2019-03-21 12:15:49 +0300

    Работа с DOM

---
+++ lession 5/Для ДЗ №5/js/script.js
@@ -0,0 +1,13 @@
+let li_3 =  document.getElementsByTagName('li')[2],
+    adv = document.querySelectorAll('.column'),
+    delete_adv = document.querySelector('.adv'),
+    title = document.getElementById('title'),
+    que = document.getElementById('prompt'),
+    menu = document.querySelector('ul');
+
+console.log(title);
+menu.insertBefore(li_3, menu.children[1]);
+document.body.style.background = 'url(/lession%205/%D0%94%D0%BB%D1%8F%20%D0%94%D0%97%20%E2%84%965/img/apple_true.jpg)';
+adv[1].removeChild(delete_adv);
+title.innerHTML = "Мы продаем только <b>подлиную</b> технику Apple";
+que.textContent = prompt("Как вы относитесь в технике Apple?", "");
